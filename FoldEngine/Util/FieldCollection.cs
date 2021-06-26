da6b4815f7f13482430292debb1860f8b0225667
Energyxxer <energyxxer@gmail.com>
2021-06-26 19:23:31 -0400

    Add FieldCollection

---
+++ FoldEngine/Util/FieldCollection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace EntryProject.Util {
+    public class FieldCollection {
+        private Dictionary<IField, object> _dict = new Dictionary<IField,object>();
+
+        public T Get<T>(Field<T> field) {
+            if(_dict.ContainsKey(field)) {
+                return _dict[field] is T t ? t : default;
+            }
+            return default;
+        }
+
+        public bool Has<T>(Field<T> field) {
+            return _dict.ContainsKey(field);
+        }
+
+        public void Set<T>(Field<T> field, T value) {
+            _dict[field] = value;
+        }
+    }
+
+    public interface IField {
+        
+    }
+
+    public class Field<T> : IField {
+        
+    }
+}
