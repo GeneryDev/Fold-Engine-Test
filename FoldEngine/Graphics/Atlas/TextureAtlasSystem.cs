b13759f655a2dd9927412edc35e18d538d8ac869
Energyxxer <energyxxer@gmail.com>
2022-04-03 15:21:05 -0400

    Make atlas system keep textures loaded during creation

--- FoldEngine/Graphics/Atlas/TextureAtlasSystem.cs
+++ FoldEngine/Graphics/Atlas/TextureAtlasSystem.cs
@@ -21,32 +21,45 @@ namespace FoldEngine.Graphics.Atlas {
                 if(atlasComponent.Generated) continue;
                 if(atlasComponent.Group == null || !atlasComponent.Group.StartsWith("#")) continue;
                 if(Scene.Core.ResourceIndex.Exists<TextureR>(atlasComponent.Group)) {
-                    Console.WriteLine("Group " + atlasComponent.Group + " exists!");
                     if(atlasComponent.Atlas == null) {
                         atlasComponent.Atlas = new TextureAtlas(atlasComponent.Group, Scene.Resources);
                         
                         foreach(string rawIdentifier in Scene.Core.ResourceIndex.GetIdentifiersInGroup<TextureR>(atlasComponent
                             .Group)) {
-                            Console.WriteLine("Waiting for texture " + rawIdentifier);
                             atlasComponent.WaitingForLoad++;
                             var identifier = new ResourceIdentifier(rawIdentifier);
                             Scene.Resources.Load<TextureR>(ref identifier, texture => {
                                 ref TextureAtlasComponent atlasComponent1 = ref Scene.Components.GetComponent<TextureAtlasComponent>(entityId);
                                 atlasComponent1.WaitingForLoad--;
-                                Console.WriteLine("Texture loaded: " + rawIdentifier);
                                 atlasComponent1.Atlas.AddTexture(texture.Identifier, (TextureR) texture);
                             });
                         }
                     } else if(atlasComponent.WaitingForLoad == 0) {
-                        Console.WriteLine("Generated atlas " + atlasComponent.Group);
                         atlasComponent.Atlas.Pack();
+                        Console.WriteLine("Generated atlas " + atlasComponent.Group);
                         atlasComponent.Generated = true;
                         atlasComponent.Atlas = null;
                     }
                 }
             }
         }
-        
-        
+
+        public override void PollResources() {
+            _atlases.Reset();
+            while(_atlases.Next()) {
+                ref TextureAtlasComponent atlasComponent = ref _atlases.GetComponent();
+                if(atlasComponent.Generated) continue;
+                if(atlasComponent.Group == null || !atlasComponent.Group.StartsWith("#")) continue;
+                if(atlasComponent.Atlas == null) continue;
+                
+                if(Scene.Core.ResourceIndex.Exists<TextureR>(atlasComponent.Group)) {
+                    foreach(string rawIdentifier in Scene.Core.ResourceIndex.GetIdentifiersInGroup<TextureR>(atlasComponent
+                        .Group)) {
+                        var identifier = new ResourceIdentifier(rawIdentifier);
+                        Scene.Resources.KeepLoaded<TextureR>(ref identifier);
+                    }
+                }
+            }
+        }
     }
 }
