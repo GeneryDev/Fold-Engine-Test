0e4c28a86394183b281f60cf46d79a34d9ad4cf3
Energyxxer <energyxxer@gmail.com>
2024-11-21 01:11:42 -0400

    Add viewport to editor scene

--- Engine-Fold/Editor/EditorScene.cs
+++ Engine-Fold/Editor/EditorScene.cs
@@ -1,4 +1,6 @@
-﻿using FoldEngine.Interfaces;
+﻿using FoldEngine.Gui.Components;
+using FoldEngine.Gui.Systems;
+using FoldEngine.Interfaces;
 using FoldEngine.Scenes;
 
 namespace FoldEngine.Editor;
@@ -15,5 +17,22 @@ public class EditorScene : Scene
         Systems.Add<EditorBase>();
         Systems.Add<EditorCameraSyncSystem>();
         Systems.Add<SubSceneSystem>();
+        
+        Systems.Add<ControlLayoutSystem>();
+        Systems.Add<ControlRenderer>();
+        Systems.Add<ControlInterfaceSystem>();
+
+        var entViewport = CreateEntity("Viewport");
+        entViewport.AddComponent<Viewport>() = new Viewport
+        {
+            RenderGroupName = "editor",
+            RenderLayerName = "editor_gui"
+        };
+        entViewport.AddComponent<Control>();
+        entViewport.AddComponent<AnchoredControl>() = new AnchoredControl()
+        {
+            AnchorRight = 1,
+            AnchorBottom = 1
+        };
     }
 }
