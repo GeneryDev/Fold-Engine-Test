666ff9ca00acd764668a4b1c6879d5a5d14bf430
Energyxxer <energyxxer@gmail.com>
2024-12-23 17:08:56 -0400

    Create ActionEvents.cs

---
+++ Engine-Fold/Input/ActionEvents.cs
@@ -0,0 +1,25 @@
+﻿using FoldEngine.Events;
+
+namespace FoldEngine.Input;
+
+[Event("fold:input_action", EventFlushMode.Immediate)]
+public struct InputActionEvent
+{
+    public string ActionName;
+    public int PlayerIndex;
+    public InputActionTriggerCause TriggerCause;
+    public bool Consumed;
+
+    public void Consume()
+    {
+        Consumed = true;
+    }
+}
+
+public enum InputActionTriggerCause
+{
+    Tap,
+    Press,
+    Echo,
+    Release
+}
